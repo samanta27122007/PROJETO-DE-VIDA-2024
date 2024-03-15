e448ff260003070cf7155dfc2211010bfa9c2aeb
samanta27122007 <148972008+samanta27122007@users.noreply.github.com>
2024-03-15 11:00:23 -0300

    aula3

---
+++ samanta/PROJETO-DE-VIDA-2024-main/main.js
@@ -0,0 +1,8 @@
+const botoes = document.querySelectorAll("bota");
+
+for(let i=0;i <botoes.length;i++){
+    botoes{i}.onclik = function(){
+botoes [i].classList.add("ativo");
+}
+    console.log(i);
+
